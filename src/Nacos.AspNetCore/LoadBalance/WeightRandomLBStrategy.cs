a99af569e870ac5fe90c697b04fd3d86ef015581
catcherwong <catcher_hwq@outlook.com>
2020-06-16 00:07:56 +0800

    :sparkles: impl lb strategy

--- src/Nacos.AspNetCore/LoadBalance/WeightRandomLBStrategy.cs
+++ src/Nacos.AspNetCore/LoadBalance/WeightRandomLBStrategy.cs
@@ -1,6 +1,8 @@
 ﻿namespace Nacos.AspNetCore
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class WeightRandomLBStrategy : ILBStrategy
     {
@@ -8,7 +10,37 @@
 
         public string GetInstance(List<NacosServer> list)
         {
-            throw new System.NotImplementedException();
+            var dict = BuildScore(list);
+
+            var instance = string.Empty;
+
+            var rd = new Random().NextDouble();
+
+            foreach (var item in dict)
+            {
+                if (item.Value >= rd)
+                {
+                    instance = item.Key;
+                    break;
+                }
+            }
+
+            return instance;
+        }
+
+        private Dictionary<string, double> BuildScore(List<NacosServer> list)
+        {
+            var dict = new Dictionary<string, double>();
+            var total = list.Sum(x => x.Weight);
+            var cur = 0d;
+
+            foreach (var item in list)
+            {
+                cur += item.Weight;
+                dict.Add(item.Url, cur / total);
+            }
+
+            return dict;
         }
     }
 }
